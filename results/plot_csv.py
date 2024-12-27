ad0e688adb3a3148d52fa171c6805c69d3d06fc6
CSSabino <sabinomalafronte27@gmail.com>
2024-12-27 12:02:18 +0100

    Adding file to generate plots
    Co-authored-by: Angelo Palmieri <palmieri309@gmail.com>
    Co-authored-by: raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>

---
+++ results/plot_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Read CSV file
+file_path = "training_results_q_learning.csv"
+df = pd.read_csv(file_path)
+
+# Create a FacetGrid to visualize the data
+g = sns.FacetGrid(
+    df, col="Exploration Probability", row="Learning Rate", margin_titles=True, height=3.5
+)
+g.map(sns.lineplot, "Discount Factor", "Percentage Win Agent", marker="o")
+
+# Adjust titles and labels
+g.set_titles(col_template="Exploration Prob={col_name}", row_template="Learning Rate={row_name}")
+g.set_axis_labels("Discount Factor", "Percentage Win Agent")
+g.set(ylim=(64, 77))  # Set y-axis limits for better visualization
+g.tight_layout()  # Adjust layout to prevent overlap
+
+# Display the plots
+plt.show()
